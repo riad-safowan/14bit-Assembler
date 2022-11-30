77b695c2f611d94176465efebd8359149417f668
Riad Safowan <riad.safowan@northsouth.edu>
2022-12-01 01:02:59 +0600

    Jump Location $ Shift amount added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,13 @@ import java.io.File
 import java.util.Scanner
 import java.util.concurrent.TimeUnit
 
+val map = loadMap()
+val binCode = StringBuilder()
+val hexCode = StringBuilder()
+val jumpLocations = mutableMapOf<String, Int>()
+
 fun main() {
     println("Program Started\n")
-    val map = loadMap()
 
     val assembly = "assembly.txt"
     val machineBin = "binary.txt"
@@ -14,8 +18,6 @@ fun main() {
     val binaryFile = File(machineBin)
     val hexFile = File(machineHex)
 
-    val binCode = StringBuilder()
-    val hexCode = StringBuilder()
 
     var instructionCount = 0
 
@@ -23,31 +25,35 @@ fun main() {
         TimeUnit.MILLISECONDS.sleep(500L)
         println("Loading Assembly code...")
         TimeUnit.MILLISECONDS.sleep(1500L)
-        assemblyFile.forEachLine {
-            println(it)
+        var line = 0
+        assemblyFile.forEachLine { instruction ->
+            println(instruction)
+            for (i in instruction.indices) {
+                if (instruction[i] == ' ') {
+                    val subStr = instruction.substring(0, i).deductCommaOrColon()
+                    if (instructionFormat(subStr) == 'L') {
+                        jumpLocations[subStr.deductCommaOrColon()] = line
+                    }
+                    break
+                }
+            }
+            line++
         }
+
         TimeUnit.MILLISECONDS.sleep(500L)
         println("\nGenerating Binary code...")
         TimeUnit.MILLISECONDS.sleep(1500L)
         assemblyFile.forEachLine { instruction ->
-            var startPos = 0
-            for (i in instruction.indices) {
-                if (instruction[i] == ' ') {
-                    val subStr = instruction.substring(startPos, i).deductComma()
-                    startPos = i + 1
-                    binCode.append(map[subStr] + " ")
-                    hexCode.append(map[subStr]?.binToHex())
+            when (getFormat(instruction)) {
+                'R', 'I' -> {
+                    generateBinSeq(instruction)
                 }
-                if (i == instruction.length - 1) {
-                    val subStr = instruction.substring(startPos, i + 1)
-                    try {
-                        val num = subStr.toInt()
-                        val bin = decimalTo4bitBinary(num)
-                        binCode.append(bin + "\n")
-                        hexCode.append(bin.binToHex() + "\n")
-                    } catch (e: Exception) {
-                        map[subStr]?.let { binCode.append(it + "\n") }
-                        hexCode.append(map[subStr]?.binToHex() + "\n")
+                'L' -> {
+                    for (i in instruction.indices) {
+                        if (instruction[i] == ' ') {
+                            generateBinSeq(instruction.substring(i + 1))
+                            break
+                        }
                     }
                 }
             }
@@ -59,6 +65,7 @@ fun main() {
         println("Transferred Binary code to \"$machineBin\" file")
         TimeUnit.MILLISECONDS.sleep(2000L)
         println("\nGenerating Hex code...")
+        generateHexCode(binCode.toString())
         TimeUnit.MILLISECONDS.sleep(1500L)
         print(hexCode)
         hexFile.writeText(hexCode.toString())
@@ -78,12 +85,52 @@ fun main() {
     reader.nextLine()
 }
 
-fun String.deductComma(): String {
-    return if (this.last() == ',')
-        this.substring(0, this.length - 1)
-    else
-        this
+fun generateHexCode(bin: String) {
+    for (i in 0..bin.length - 14 step 15) {
+        hexCode.append("00${bin.substring(i + 0, i + 2)}".binToHex())
+        hexCode.append(bin.substring(i + 2, i + 6).binToHex())
+        hexCode.append(bin.substring(i + 6, i + 10).binToHex())
+        hexCode.append(bin.substring(i + 10, i + 14).binToHex() + "\n")
+    }
 }
+
+fun generateBinSeq(instruction: String) {
+    var startPos = 0
+    for (i in instruction.indices) {
+        if (instruction[i] == ' ') {
+            val subStr = instruction.substring(startPos, i).deductCommaOrColon()
+            startPos = i + 1
+            binCode.append(map[subStr])
+        }
+        if (i == instruction.length - 1) {
+            val subStr = instruction.substring(startPos, i + 1)
+            try {
+                val num = subStr.toInt()
+                val bin = decimalTo4bitBinary(num)
+                binCode.append(bin + "\n")
+            } catch (e: Exception) {
+                if (!map[subStr].isNullOrEmpty()) {
+                    binCode.append(map[subStr] + "0\n")
+                } else binCode.append(jumpLocations[subStr]?.let { decimalTo4bitBinary(it) + "\n" })
+            }
+        }
+    }
+}
+
+fun getFormat(s: String): Char {
+    var firstWord = ""
+    for (i in s.indices) {
+        if (s[i] == ' ') {
+            firstWord = s.substring(0, i).deductCommaOrColon()
+            break
+        }
+    }
+    return instructionFormat(firstWord)
+}
+
+fun String.deductCommaOrColon() = if (this.last() == ',' || this.last() == ':')
+    this.substring(0, this.length - 1)
+else this
 
 fun decimalTo4bitBinary(d: Int): String {
     var decimal = d
@@ -94,6 +141,13 @@ fun decimalTo4bitBinary(d: Int): String {
     }
     return b.reverse().toString()
 }
+
+fun instructionFormat(s: String) = when (s) {
+    "Add", "Sub", "AND", "Sll", "Slt" -> 'R'
+    "Addi", "Beq", "Slti", "J", "Sw", "Lw" -> 'I'
+    else -> 'L'
+}
+
 
 fun String.binToHex(): String {
     return when (this) {
@@ -123,7 +177,6 @@ fun loadMap() = mutableMapOf(
     "Sub" to "0001",
     "Addi" to "0010",
     "AND" to "0011",
-    "add" to "0110",
     "Sll" to "0111",
     "Beq" to "1001",
     "Slt" to "1011",
@@ -132,12 +185,12 @@ fun loadMap() = mutableMapOf(
     "Sw" to "1110",
     "Lw" to "1111",
     //Register
-    "\$zero" to "0000",
-    "\$t0" to "0001",
-    "\$t1" to "0010",
-    "\$t2" to "0011",
-    "\$s0" to "0100",
-    "\$s1" to "0101",
-    "\$s2" to "0110",
-    "\$s3" to "0111",
+    "\$zero" to "000",
+    "\$t0" to "001",
+    "\$t1" to "010",
+    "\$t2" to "011",
+    "\$s0" to "100",
+    "\$s1" to "101",
+    "\$s2" to "110",
+    "\$s3" to "111",
 )
